e06dc59f44c69acc7e51513b7a33576e52bc4442
pat <pat@quora.com>
2018-09-19 10:31:10 -0700

    version 0.1
    Working version of the chrome extension.

---
+++ js/rss.js
@@ -0,0 +1,33 @@
+var feed = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+$.ajax(feed, {
+	accepts:{
+		xml:'application/rss+xml'
+	},
+	dataType:'xml',
+	success:function(data) {
+		$(data).find('item').each(function () {
+			var el = $(this);
+			var url = el.find("link").text();
+			if (url.indexOf("opinion") != -1) { 
+				// skip because opinion articles contain bylines
+			} else {
+				url = url.split('?')[0];
+				var author_string = el.find('dc\\:creator').text().toLowerCase();
+
+				var split_string = author_string.toLowerCase().split(' ');
+				for (var i = 0; i < split_string.length; i++) {
+					if (split_string[i] == 'and') {
+						split_string[i] = split_string[i].charAt(0) + split_string[i].substring(1);
+					} else {
+						split_string[i] = split_string[i].charAt(0).toUpperCase() + split_string[i].substring(1);
+					}  
+				}
+			    author_string = split_string.join(' ');
+				var url_search_query = 'a[href*=' + '"' + url + '"' + ']';
+				var story_wrapper = $(url_search_query);
+				console.log(story_wrapper);
+				story_wrapper.append('<div class="story_byline">' + author_string + '</div>');
+			}
+		});
+	}	
+});
